28098f0a3716db8320806806f213ebbec35a1b17
Gangadhar Parde <gangadhar.parde@gmail.com>
2022-05-08 15:37:29 +0530

    First push of skeleton code

---
+++ src/main/java/com/gd/hotel18degree/Hotel18DegreeApplication.java
@@ -0,0 +1,13 @@
+package com.gd.hotel18degree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hotel18DegreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hotel18DegreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gd/hotel18degree/Hotel18DegreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gd.hotel18degree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hotel18DegreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
